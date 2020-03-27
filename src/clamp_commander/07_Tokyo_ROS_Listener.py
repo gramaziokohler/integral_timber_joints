8e2e2e327ecb9b56ced37fd3abb2845617152867
yck011522 <yck011522@gmail.com>
2020-03-27 14:15:58 +0100

    RTT Measurement

--- src/clamp_commander/07_Tokyo_ROS_Listener.py
+++ src/clamp_commander/07_Tokyo_ROS_Listener.py
@@ -10,14 +10,24 @@ client.run()
 trip_times = []
 def callback(message_string):
     message = json.loads(message_string['data'])
+    # Reply to the message.
+    reply_message = {}
+    reply_message['sequence_id'] = message ['sequence_id'] 
+    reply_message['timestamp'] = current_milli_time()
+    replier.publish(roslibpy.Message({'data' : json.dumps(reply_message)}))
+    # Print it to UI and keep track of one way latency.
     print (message)
     trip_time = current_milli_time() - int(message['timestamp'])
     trip_times.append(trip_time)
     print ("Trip Time = %s" % trip_time)
 
+# Setup listener and replier topics.
 listener = roslibpy.Topic(client, '/clamp_command', 'std_msgs/String')
 listener.subscribe(callback)
-    
+replier = roslibpy.Topic(client, '/clamp_response', 'std_msgs/String')
+replier.advertise()
+
+# Allow user to terminate via CLI
 while (client.is_connected):
     i = input("Press x to quit and see average trip time. \n")
     if i == 'x': break
