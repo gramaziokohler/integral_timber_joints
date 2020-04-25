5db180e527ed2139150d5e03fb69d4cd4260fb3b
Victor Leung <yck011522@gmail.com>
2020-04-25 11:48:18 +0200

    Added Yijiang's blocking direction implementation.

---
+++ tests/test_blocking.py
@@ -0,0 +1,33 @@
+# from geometric_blocking import compute_feasible_region_from_block_dir
+
+
+from compas.geometry import Vector
+from compas.rpc import Proxy
+
+def compute_feasible_region_from_block_dir_proxy(block_dirs):
+    geometric_blocking = Proxy(package='geometric_blocking')
+    return geometric_blocking.compute_feasible_region_from_block_dir(block_dirs)
+
+
+def test_proxycall_geometric_blocking():
+    block_dirs = [[0,0,1], [0,0,-1.0]]
+    f_rays, lin_set = compute_feasible_region_from_block_dir_proxy(block_dirs)
+
+    assert [1.0, 0.0, 0.0] in f_rays
+    assert [0.0, 1.0, 0.0] in f_rays
+    assert 0 in lin_set
+    assert 1 in lin_set
+
+def test_proxycall_geometric_blocking_compas_vector():
+    block_dirs = [Vector(0,0,1), Vector(0,0,-1.0)]
+    f_rays, lin_set = compute_feasible_region_from_block_dir_proxy(block_dirs)
+
+    assert [1.0, 0.0, 0.0] in f_rays
+    assert [0.0, 1.0, 0.0] in f_rays
+    assert 0 in lin_set
+    assert 1 in lin_set
+
+if __name__ == "__main__":
+    test_proxycall_geometric_blocking()
+    test_proxycall_geometric_blocking_compas_vector()
+    pass
