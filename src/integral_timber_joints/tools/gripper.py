e66b07af64c47ada5086aeef4a032fe660ea51d4
Victor Leung <yck011522@gmail.com>
2020-07-09 17:30:08 +0200

    Parallel Gripper PG500 and PG1000 are modeled. Json is created. MultiFingerParallelGripperFactory is created for better collision detection.

--- src/integral_timber_joints/tools/gripper.py
+++ src/integral_timber_joints/tools/gripper.py
@@ -44,8 +44,9 @@ class Gripper (Tool):
     @gripper_jaw_position.setter
     def gripper_jaw_position(self, position):
         self._gripper_jaw_position = position
-        self.get_joint_by_name('joint_gripper_jaw_l').position = position
-        self.get_joint_by_name('joint_gripper_jaw_r').position = position
+        for joint in self.get_configurable_joints():
+            if joint.name.startswith('joint_gripper_'):
+                joint.position = position
 
     def _set_kinematic_state(self, state_dict):
         self.gripper_jaw_position = state_dict['gripper_jaw_position']
@@ -104,6 +105,56 @@ def ParallelGripperFactory(
     #robot_model.add_joint('world_base_fixed_joint', Joint.FIXED, world_link, base_link)
     robot_model.add_joint('joint_gripper_jaw_l', Joint.PRISMATIC, gripper_base, gripper_jaw_l, axis = [0,-1,0], limit = robot_model.gripper_jaw_limits)
     robot_model.add_joint('joint_gripper_jaw_r', Joint.PRISMATIC, gripper_base, gripper_jaw_r, axis = [0,1,0], limit = robot_model.gripper_jaw_limits)
+
+    return robot_model
+
+def MultiFingerParallelGripperFactory(
+    name,
+    type_name,
+    gripper_jaw_position_min,
+    gripper_jaw_position_max,
+    tool_coordinate_frame,      # TCF / TCP (In Ref to T0CF)
+    tool_pick_up_frame,
+    tool_storage_frame,
+    mesh_gripper_base,
+    meshes_gripper_jaw_l,    # type: List[Mesh] # Accepts multiple meshes
+    meshes_gripper_jaw_r,    # type: List[Mesh] # Accepts multiple meshes
+    jaw_vector_l,           # In Ref to T0CF
+    jaw_vector_r,           # In Ref to T0CF
+    approach_vector
+    ):
+    """ A Parallel gripper will have a base and two gripper jaw.
+    Modelling guide
+    ---------------
+    The left jaws opens towards -Y direction.
+    The right jaw opens towards +Y direction.
+    The gripper opening should point towards +Z direction
+    """
+    robot_model = Gripper(name,type_name)
+    robot_model.gripper_jaw_limits = (gripper_jaw_position_min, gripper_jaw_position_max)
+
+    robot_model.tool_coordinate_frame = tool_coordinate_frame
+    robot_model.tool_pick_up_frame = tool_pick_up_frame
+    robot_model.tool_storage_frame = tool_storage_frame
+    robot_model.approach_vector = approach_vector       # This vector is ref to t0cf
+    # TODO: This and other paramters might need a clean up.
+
+    # Gripper Base and Jaw Links
+    #world_link = robot_model.add_link('world')
+    gripper_base = robot_model.add_link('gripper_base', mesh_gripper_base)
+    gripper_jawlinks_l = []
+    for i, mesh in enumerate(meshes_gripper_jaw_l):
+        gripper_jawlinks_l.append(robot_model.add_link('gripper_jaw_l%i'%i, mesh))
+    gripper_jawlinks_r = []
+    for i, mesh in enumerate(meshes_gripper_jaw_r):
+        gripper_jawlinks_r.append(robot_model.add_link('gripper_jaw_r%i'%i, mesh))
+
+    # Jaw Joints
+    #robot_model.add_joint('world_base_fixed_joint', Joint.FIXED, world_link, base_link)
+    for link in gripper_jawlinks_l:
+        robot_model.add_joint('joint_%s'%link.name, Joint.PRISMATIC, gripper_base, link, axis = jaw_vector_l, limit = robot_model.gripper_jaw_limits)
+    for link in gripper_jawlinks_r:
+        robot_model.add_joint('joint_%s'%link.name, Joint.PRISMATIC, gripper_base, link, axis = jaw_vector_r, limit = robot_model.gripper_jaw_limits)
 
     return robot_model
 
