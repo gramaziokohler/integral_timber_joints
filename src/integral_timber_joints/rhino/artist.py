ece2ebe4f0b47f15fd626298427aca9b53829095
Victor LEUNG <yck011522@gmail.com>
2020-10-09 17:16:02 +0200

    Artist to draw along sequence. Draw block test still going on

--- src/integral_timber_joints/rhino/artist.py
+++ src/integral_timber_joints/rhino/artist.py
@@ -1,10 +1,10 @@
 from compas.datastructures.mesh import Mesh
-from integral_timber_joints.geometry import Beam
+from integral_timber_joints.geometry import Beam, Frame
 from compas_rhino.utilities import delete_objects, clear_layer
 from compas_rhino.utilities import draw_mesh
 from integral_timber_joints.geometry import beam
 from integral_timber_joints.process import RobotClampAssemblyProcess
-from integral_timber_joints.assembly import Assembly
+from integral_timber_joints.assembly import Assembly, assembly
 
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
@@ -62,7 +62,7 @@ class ProcessArtist(object):
             'show.edges': True,
             'show.faces': True}
 
-    def draw_beam_id(self, beam_id, faces = [1,3], padding_factor = 0.2, size_factor = 0.6):
+    def draw_beam_seqtag(self, beam_id, faces = [1,3], padding_factor = 0.2, size_factor = 0.6):
         assembly = self.process.assembly
         seq_num = assembly.sequence.index(beam_id)
         for face_id in faces:
@@ -81,7 +81,7 @@ class ProcessArtist(object):
             tag_height = beam_size_min * size_factor
 
             # Create Tag
-            layer ='itj::beams_mesh'
+            layer ='itj::beams_seqtag'
             guid = AddAnnotationText(face_frame, tag_text, tag_height, layer)
             self.guids[beam_id][layer].append(guid)
 
@@ -103,21 +103,14 @@ class ProcessArtist(object):
     def redraw_beam(self, beam_id, force_update = True, draw_mesh=True, draw_tag = True):
         ''' Redraw a beam visualizations.
         '''
-        self.clear_one_beam(beam_id)
+        self.delete_one_beam(beam_id)
         if draw_mesh: self.draw_beam_mesh(beam_id, force_update)
-        if draw_tag: self.draw_beam_id(beam_id)
+        if draw_tag: self.draw_beam_seqtag(beam_id)
 
     def draw_beam_brep(self, process):
         raise NotImplementedError
 
-    def clear_all(self):
-        """ Clear all display geometry (brep, mesh, tag etc), all beams.
-        Stored guid reference is also removed.
-        """
-        for beam_id in self.guids:
-            self.clear_one_beam(beam_id)
-
-    def clear_one_beam(self, beam_id):
+    def delete_one_beam(self, beam_id):
         # type:(str) -> None
         """ Clear all display geometry (brep, mesh, tag etc) related to a beam.
         Stored guid reference is also removed.
@@ -150,6 +143,48 @@ class ProcessArtist(object):
             for layer in self.layers:
                 self.guids[beam_id][layer] = []
 
+    def show_beam(self, beam_id):
+        """ Show the beam of the beam_id.
+        """
+        guids = []
+        for layer in self.layers:
+            for guid in self.guids[beam_id][layer]:
+                guids.append(guid) 
+        rs.ShowObject(guids)
+        # print ('Showing: %s' % guids)
+
+    def hide_beam(self, beam_id):
+        """ Show the beam of the beam_id.
+        """
+        guids = []
+        for layer in self.layers:
+            for guid in self.guids[beam_id][layer]:
+                guids.append(guid) 
+        rs.HideObject(guids)
+        # print ('Hiding: %s' % guids)
+
+    def show_until(self, beam_id):
+        """ Show only the beams before and the given beam_id, others are hidden.
+        If beam_id is None, all beams are hidden.
+        If beam_id is not in the list of sequence, all beams are shown.
+        """
+
+        assembly = self.process.assembly
+        show = True
+        if beam_id is None: show = False
+        for _beam_id in assembly.sequence:
+            if show:
+                self.show_beam(_beam_id)
+            else:
+                self.hide_beam(_beam_id)
+            if _beam_id == beam_id:
+                show = False
+
+    # def draw_gripper_block(self, beam_id):
+    #     """Draw the components of a gripper as a block in Rhino for fast display and 
+    #     """
+
+
 if __name__ == "__main__":
 
     from integral_timber_joints.rhino.load import load_process, get_process, get_process_artist
@@ -157,8 +192,26 @@ if __name__ == "__main__":
     # load_process()
 
     artist = get_process_artist()
-    artist.clear_all_layers()
+    # artist.empty_layers()
     process = get_process()
-    for beam_id in process.assembly.beam_ids():
-        artist.draw_beam_mesh(beam_id)
-    artist.clear_one_beam('b15')
+    # for beam_id in process.assembly.beam_ids():
+    #     artist.draw_beam_mesh(beam_id)
+    #     artist.draw_beam_seqtag(beam_id)
+    # artist.delete_one_beam('b15')
+    # artist.hide_beam('b34')
+    # artist.show_beam('b34')
+    # artist.show_until('b25')
+    """Draw the clamp as a block. Block name is the clamp_type"""
+    for clamp_type in process.available_clamp_types:
+        clamp = process.get_one_clamp_by_type(clamp_type).copy()
+        clamp.open_gripper()
+        clamp.open_clamp()
+        clamp.current_frame = Frame.worldXY()
+        clamp_meshes = clamp.draw_visuals()
+        guids = []
+        for clamp_mesh in clamp_meshes:
+            v, f = clamp_mesh.to_vertices_and_faces()
+            layer ='itj::clamps'
+            guids.append(draw_mesh(v, f, name=clamp_type, layer = layer))
+        block = rs.AddBlock(guids, (0,0,0), clamp_type, True)
+        #rs.InsertBlock(block, (10,10,10))
