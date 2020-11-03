c0a740db6fade08744deb1131f46ff7382a6e28e
Victor LEUNG <yck011522@gmail.com>
2020-11-03 19:14:25 +0100

    Lint

--- src/integral_timber_joints/rhino/artist.py
+++ src/integral_timber_joints/rhino/artist.py
@@ -1,31 +1,31 @@
-from compas.datastructures.mesh import Mesh
-from integral_timber_joints.geometry import Beam, Frame
-from compas_rhino.utilities import delete_objects, clear_layer
-from compas_rhino.utilities import draw_mesh
-from integral_timber_joints.geometry import beam
-from integral_timber_joints.process import RobotClampAssemblyProcess
-from integral_timber_joints.assembly import Assembly, assembly
-
+import Rhino
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
-import Rhino
 
-from compas_rhino.geometry import RhinoPlane
+from compas.datastructures import Mesh
 from compas.geometry import Frame
+from compas_rhino.geometry import RhinoPlane
+from compas_rhino.utilities import clear_layer, delete_objects, draw_mesh
+
+from integral_timber_joints.assembly import Assembly
+from integral_timber_joints.geometry import Beam
+from integral_timber_joints.process import RobotClampAssemblyProcess
+
 
 def AddAnnotationText(frame, text, height, layer):
-    font = "Arial"  
+    font = "Arial"
     plane = RhinoPlane.from_geometry(frame).geometry
     justification = Rhino.Geometry.TextJustification.BottomLeft
     guid = sc.doc.Objects.AddText(text, plane, height, font, False, False, justification)
     import System.Guid
-    if guid!=System.Guid.Empty:
+    if guid != System.Guid.Empty:
         rs.ObjectLayer(guid, layer)
         # o = sc.doc.Objects.FindId(rs.coerceguid(guid))
         # o.Geometry.TextOrientation = Rhino.DocObjects.TextOrientation.InPlane
         # o.CommitChanges()
         sc.doc.Views.Redraw()
         return guid
+
 
 class ProcessArtist(object):
     """ Artist to draw Beams in Rhino
@@ -36,29 +36,37 @@ class ProcessArtist(object):
     self.guids is a dictionary that keeps the guids for objects drawn in Rhino 
     """
 
-    layers = ['itj::beams_brep', 'itj::beams_mesh', 'itj::beams_seqtag']
+    # Rhino layers used to hold temporary visualization objects
+    layers = [
+        'itj::beams_brep',
+        'itj::beams_mesh',
+        'itj::beams_seqtag',
+        'itj::clamps_at_final',
+    ]
+
     color_meaning = {
-        'normal' : (0,0,0),
-        'warning' : (255, 152, 0),
-        'error' : (244, 67, 54),
-        'active' : (76, 175, 80),
-        'unbuilt' : (220,220,220),
-        'unbuilt_warning': (255,204,204),
-        'built' : (61, 167, 219),
-        'built_warning': (130,20,20),       
-        'neighbors' : (0, 188, 212),
+        'normal': (0, 0, 0),
+        'warning': (255, 152, 0),
+        'error': (244, 67, 54),
+        'active': (76, 175, 80),
+        'unbuilt': (220, 220, 220),
+        'unbuilt_warning': (255, 204, 204),
+        'built': (61, 167, 219),
+        'built_warning': (130, 20, 20),
+        'neighbors': (0, 188, 212),
     }
+
     def __init__(self, process):
         # type: (RobotClampAssemblyProcess) -> None
-        self.process = process # type: RobotClampAssemblyProcess
+        self.process = process  # type: RobotClampAssemblyProcess
 
         # Store guid in dictionary
-        self.guids = {} # type: dict[str, dict[str, list[str]]]
+        self.guids = {}  # type: dict[str, dict[str, list[str]]]
         for beam_id in process.assembly.beam_ids():
             self.guids[beam_id] = {}
             for layer in self.layers:
                 self.guids[beam_id][layer] = []
-        print (self.guids)
+        # print(self.guids)
 
         self.settings = {
             'color.vertex': (255, 255, 255),
@@ -72,50 +80,58 @@ class ProcessArtist(object):
             'show.edges': True,
             'show.faces': True}
 
-    def draw_beam_seqtag(self, beam_id, faces = [1,3], padding_factor = 0.2, size_factor = 0.6):
+        self.empty_layers()
+
+    ######################
+    # Beam
+    ######################
+
+    def draw_beam_seqtag(self, beam_id, faces=[1, 3], padding_factor=0.2, size_factor=0.6):
         assembly = self.process.assembly
         seq_num = assembly.sequence.index(beam_id)
         for face_id in faces:
             # Get Face Frame
             beam = assembly.beam(beam_id)
             face_frame = beam.reference_side_wcf(face_id)
-            
+
             # Move Frame orgin for padding
             beam_size_min = min(beam.height, beam.width)
             padding = beam_size_min * padding_factor
-            padded_location_origin = face_frame.to_world_coordinates([padding, padding , beam_size_min * 0.01])
+            padded_location_origin = face_frame.to_world_coordinates([padding, padding, beam_size_min * 0.01])
             face_frame.point = padded_location_origin
-         
+
             # Test and Size
             tag_text = ".%s." % seq_num
             tag_height = beam_size_min * size_factor
 
             # Create Tag
-            layer ='itj::beams_seqtag'
+            layer = 'itj::beams_seqtag'
             guid = AddAnnotationText(face_frame, tag_text, tag_height, layer)
             self.guids[beam_id][layer].append(guid)
 
-    def draw_beam_mesh(self, beam_id, update_cache = False):
+    def draw_beam_mesh(self, beam_id, update_cache=False):
         # type:(str, bool) -> None
         assembly = self.process.assembly
         if beam_id not in assembly.beam_ids():
             raise KeyError("Beam %i not in Assembly" % beam_id)
         assembly.update_beam_mesh_with_joints(beam_id, not update_cache)
-        beam_mesh = assembly.beam(beam_id).cached_mesh # type: Mesh
+        beam_mesh = assembly.beam(beam_id).cached_mesh  # type: Mesh
         v, f = beam_mesh.to_vertices_and_faces()
-        layer ='itj::beams_mesh'
-        guid = draw_mesh(v, f, name=beam_id, layer = layer)
+        layer = 'itj::beams_mesh'
+        guid = draw_mesh(v, f, name=beam_id, layer=layer)
 
         # print (self.guids)
         # print (self.guids[beam_id][layer])
         self.guids[beam_id][layer].append(guid)
 
-    def redraw_beam(self, beam_id, force_update = True, draw_mesh=True, draw_tag = True):
+    def redraw_beam(self, beam_id, force_update=True, draw_mesh=True, draw_tag=True):
         ''' Redraw a beam visualizations.
         '''
         self.delete_one_beam(beam_id)
-        if draw_mesh: self.draw_beam_mesh(beam_id, force_update)
-        if draw_tag: self.draw_beam_seqtag(beam_id)
+        if draw_mesh:
+            self.draw_beam_mesh(beam_id, force_update)
+        if draw_tag:
+            self.draw_beam_seqtag(beam_id)
 
     def draw_beam_brep(self, process):
         raise NotImplementedError
@@ -148,10 +164,24 @@ class ProcessArtist(object):
             else:
                 clear_layer(layer)
 
-        # clear out saved guids. 
+        # clear out saved guids.
         for beam_id in self.process.assembly.beam_ids():
             for layer in self.layers:
                 self.guids[beam_id][layer] = []
+
+    ######################
+    # Clamp and Gripper
+    ######################
+
+    # def draw_clamp(self, beam_id, location):
+
+    ######################
+    # Robot
+    ######################
+
+    ######################
+    # Show Hide Color
+    ######################
 
     def show_beam(self, beam_id):
         """ Show the beam of the beam_id.
@@ -159,7 +189,7 @@ class ProcessArtist(object):
         guids = []
         for layer in self.layers:
             for guid in self.guids[beam_id][layer]:
-                guids.append(guid) 
+                guids.append(guid)
         rs.ShowObject(guids)
         # print ('Showing: %s' % guids)
 
@@ -169,7 +199,7 @@ class ProcessArtist(object):
         guids = []
         for layer in self.layers:
             for guid in self.guids[beam_id][layer]:
-                guids.append(guid) 
+                guids.append(guid)
         rs.HideObject(guids)
         # print ('Hiding: %s' % guids)
 
@@ -181,7 +211,8 @@ class ProcessArtist(object):
 
         assembly = self.process.assembly
         show = True
-        if beam_id is None: show = False
+        if beam_id is None:
+            show = False
         for _beam_id in assembly.sequence:
             if show:
                 self.show_beam(_beam_id)
@@ -196,16 +227,13 @@ class ProcessArtist(object):
         """
         for layer in ['itj::beams_brep', 'itj::beams_mesh']:
             for guid in self.guids[beam_id][layer]:
-                rs.ObjectColor(guid, self.color_meaning.get(meaning, (0,0,0)))
-                
-    # def draw_gripper_block(self, beam_id):
-    #     """Draw the components of a gripper as a block in Rhino for fast display and 
-    #     """
+                rs.ObjectColor(guid, self.color_meaning.get(meaning, (0, 0, 0)))
 
 
 if __name__ == "__main__":
 
-    from integral_timber_joints.rhino.load import load_process, get_process, get_process_artist
+    from integral_timber_joints.rhino.load import get_process, get_process_artist, load_process
+
     # Draw beams to canvas
     # load_process()
 
@@ -229,7 +257,7 @@ if __name__ == "__main__":
         guids = []
         for clamp_mesh in clamp_meshes:
             v, f = clamp_mesh.to_vertices_and_faces()
-            layer ='itj::clamps'
-            guids.append(draw_mesh(v, f, name=clamp_type, layer = layer))
-        block = rs.AddBlock(guids, (0,0,0), clamp_type, True)
+            layer = 'itj::clamps'
+            guids.append(draw_mesh(v, f, name=clamp_type, layer=layer))
+        block = rs.AddBlock(guids, (0, 0, 0), clamp_type, True)
         #rs.InsertBlock(block, (10,10,10))
