8f5af379cb3f0a303a9c5b8468bc7114748d2f87
Victor LEUNG <yck011522@gmail.com>
2021-01-08 13:47:35 +0100

    Pick up station serializable and definable from Rhino

---
+++ src/integral_timber_joints/rhino/pickup.py
@@ -0,0 +1,134 @@
+import json
+import os
+
+import Rhino
+import rhinoscriptsyntax as rs
+from compas.utilities import DataDecoder
+from compas_rhino.ui import CommandMenu
+from compas_rhino.geometry import RhinoMesh, RhinoPoint
+
+from compas_rhino.utilities.objects import get_object_name
+
+from integral_timber_joints.assembly import Assembly
+from integral_timber_joints.process import RobotClampAssemblyProcess
+from integral_timber_joints.rhino.load import get_process, get_process_artist, process_is_none
+from integral_timber_joints.rhino.utility import get_existing_beams_filter, recompute_dependent_solutions
+from integral_timber_joints.tools import Clamp, Gripper, PickupStation, StackedPickupStation
+
+from compas.geometry.primitives.frame import Frame
+from compas.geometry.primitives.vector import Vector
+
+def create_pickup_station(process):
+    # type: (RobotClampAssemblyProcess) -> None
+    station = PickupStation()
+
+    # Ask user for collision model
+    guids = rs.GetObjects("Pick mesh(es) as collision objects.", preselect=False, select=False, group=False, filter=rs.filter.mesh)
+    for guid in guids:
+        cm = RhinoMesh.from_guid(guid).to_compas()
+        station.collision_meshes.append(cm)
+        print (cm)
+
+    # frame = Frame() # type: compas.geometry.primitives.frame.Frame
+    
+    # Ask for origin
+    origin = rs.GetPoint("Pick point as alignment origin.")
+    # Ask for X axis and Y Axis
+    x_point = rs.GetPoint("Pick point on X direction.")
+    y_point = rs.GetPoint("Pick point on Y direction.")
+    
+    # Alignment frame
+    alignment_frame = Frame(origin, x_point - origin, y_point - origin)
+    station.alignment_frame = alignment_frame
+    print (alignment_frame)
+
+    # Ask for pickup retract direction
+    retraction_point = rs.GetPoint("Pick point on retraction direction at correct distance from origin.")
+    pickup_retract_vector = Vector.from_start_end(origin, retraction_point)
+    station.pickup_retract_vector = pickup_retract_vector
+
+    # Set X Y Z alignment
+
+    # Save station to process
+    process.pickup_station = station
+
+
+def create_stacked_pickup_station(process):
+    pass
+
+def import_pickup_station(process):
+    pass
+
+def export_pickup_station(process):
+    pass
+
+def not_implemented(process):
+    #
+    print('This function is not implemented')
+
+
+def show_menu(process):
+    # type: (RobotClampAssemblyProcess) -> None
+    assembly = process.assembly  # type: Assembly
+    artist = get_process_artist()
+
+    while (True):
+        # Create Menu
+        if process.pickup_station is None:
+            message = "Material Pickup is undefined"
+        elif isinstance(process.pickup_station, StackedPickupStation):
+            message = "Material Pickup is defined from a Stacked Pickup Station"
+        elif isinstance(process.pickup_station, PickupStation):
+            message = "Material Pickup is defined from a Single-position Pickup Station"
+        else:
+            message = "Material Pickup is weird"
+
+        config = {
+            'message': message,
+            'options': [
+                {'name': 'Finish', 'action': 'Exit'
+                 },
+                {'name': 'DefinePickupStation', 'action': create_pickup_station
+                 },
+                {'name': 'DefineStackedPickupStation', 'action': create_stacked_pickup_station
+                 },
+                {'name': 'Import', 'action': import_pickup_station
+                 },
+                {'name': 'Export', 'action': export_pickup_station
+                 },
+
+            ]
+
+        }
+
+        result = CommandMenu(config).select_action()
+        # User cancel command by Escape
+        if result is None or 'action' not in result:
+            print('Exit Function')
+            return Rhino.Commands.Result.Cancel
+
+        action = result['action']
+
+        # User click Exit Button
+        if action == 'Exit':
+            print('Exit Function')
+            return Rhino.Commands.Result.Cancel
+        if action == 'Back':
+            # Back is simply redisplaying the menu again from root.
+            continue
+        else:
+            # Run the selected command
+            action(process)
+
+
+######################
+# Rhino Entry Point
+######################
+# Below is the functions that get evoked when user press UI Button
+# Put this in the Rhino button ! _-RunPythonScript 'integral_timber_joints.rhino.tools_environment.py'
+if __name__ == '__main__':
+    process = get_process()
+    if process_is_none(process):
+        print("Load json first")
+    else:
+        show_menu(process)
--- src/integral_timber_joints/tools/pickup_station.py
+++ src/integral_timber_joints/tools/pickup_station.py
@@ -1,14 +1,74 @@
 class PickupStation (object):
-    def __init__(self, alignment_frame, pickup_retract_vector):
+
+    def __init__(self, alignment_frame = None, pickup_retract_vector = None):
         # Frame (in WCF) for the beam to align to
         self.alignment_frame = alignment_frame
 
         # Vector for the robot to move (in WCF) after picking up beams
         self.pickup_retract_vector = pickup_retract_vector
 
-        self.collision_mesh = None
+        self.collision_meshes = []
 
         # Point on the beam (relative to the gripper_grasp_face) for alignment purpose
         self.align_face_X0 = True
         self.align_face_Y0 = True
         self.align_face_Z0 = True
+
+    def to_data(self):
+        return self.data
+
+    @classmethod
+    def from_data(cls, data):
+        station = cls()
+        station.data = data
+        return station
+
+    @property
+    def data(self):
+        data = {}
+        data['alignment_frame'] = self.alignment_frame
+        data['pickup_retract_vector'] = self.pickup_retract_vector
+        data['collision_meshes'] = self.collision_meshes
+        data['align_face_X0'] = self.align_face_X0
+        data['align_face_Y0'] = self.align_face_Y0
+        data['align_face_Z0'] = self.align_face_Z0
+        return data
+
+    @data.setter
+    def data(self, data):
+        self.alignment_frame = data.get('alignment_frame', None)
+        self.pickup_retract_vector = data.get('pickup_retract_vector', None)
+        self.collision_meshes = data.get('collision_meshes', [])
+        self.align_face_X0 = data.get('align_face_X0', True)
+        self.align_face_Y0 = data.get('align_face_Y0', True)
+        self.align_face_Z0 = data.get('align_face_Z0', True)
+
+class StackedPickupStation (PickupStation):
+
+    def __init__(self, alignment_frame = None, pickup_retract_vector = None):
+        # Frame (in WCF) for the beam to align to
+        super(StackedPickupStation, self).__init__(alignment_frame, pickup_retract_vector)
+        self.number_of_rows = 4
+        self.hori_spacing = 100
+        self.vert_spacing = 100
+
+    @property
+    def data(self):
+        data = super(StackedPickupStation, self).to_data()
+        data['number_of_rows'] = self.number_of_rows
+        data['hori_spacing'] = self.hori_spacing
+        data['vert_spacing'] = self.vert_spacing
+        return data
+    
+    @data.setter
+    def data(self, data):
+        super(StackedPickupStation, self).data = data
+        self.number_of_rows = data.get('number_of_rows', 4)
+        self.hori_spacing = data.get('hori_spacing', 100)
+        self.vert_spacing = data.get('vert_spacing', 100)
+
+    @classmethod
+    def from_data(cls, data):
+        station = cls()
+        station.data = data
+        return station
