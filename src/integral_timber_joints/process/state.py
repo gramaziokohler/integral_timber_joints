db6f1ff40b838fdc5fd80b8ed134104c4f88f32f
Victor Leung <yck011522@gmail.com>
2021-02-26 00:09:36 +0100

    removed StateObject.attached_to_joint

--- src/integral_timber_joints/process/state.py
+++ src/integral_timber_joints/process/state.py
@@ -6,32 +6,25 @@ class ObjectState(object):
     """ Base class of a state object.
     Note that the State object should exist in a state dictionary where the key refers to an object id.
 
-    `State.current_frame` 
+    `ObjectState.current_frame` 
     - A frame describing the loaction of an object. `None` can be used to represent an un-transformed state.
 
-    `State.kinematic_config` 
+    `ObjectState.kinematic_config` 
     - A dictionary to describe the kinematic configuration of a RobotModel or ToolModel (such as Clamp and Gripper)
     - The dictionary uses `joint names` as keys and `values` in degrees or millimeters.
     - `None` can be used for Beams and Env Objects that does not have kinematic state
 
-    `State.attached_to_robot` 
+    `ObjectState.attached_to_robot` 
     - Bool value describing if an object is attached to the robot. 
     - Note that the ToolChanger is always attached to the robot's flange. (Level 1)
     - Gripper or Clamp can be attached to a robot's ToolChanger. (Level 2)
     - Beam can only be attached to a Gripper or Clamp (Level 3)
-
-    `State.attached_to_joint` 
-    - joint_id tuple[str,str] describing if a Clamp is hanging onto a beam passively.
-    - This is used when a Clamp is left at a Beam or if a Screwdriver is attached to a flying Beam
-
-
     """
 
     def __init__(self):
         self.current_frame = None  # type: Frame
         self.kinematic_config = None  # type: ignore
         self.attached_to_robot = False  # type: bool
-        self.attached_to_joint = None  # type: tuple[str,str]
 
     def to_data(self):
         """Simpliest way to get this class serialized.
@@ -53,7 +46,6 @@ class ObjectState(object):
             'current_frame': self.current_frame,
             'kinematic_config': self.kinematic_config,
             'attached_to_robot': self.attached_to_robot,
-            'attached_to_joint': self.attached_to_joint,
         }
         return data
 
@@ -62,7 +54,6 @@ class ObjectState(object):
         self.current_frame = data.get('current_frame', None)
         self.kinematic_config = data.get('kinematic_config', None)
         self.attached_to_robot = data.get('attached_to_robot', False)
-        self.attached_to_joint = data.get('attached_to_joint', None)
 
 
 def get_object_from_flange(object_states, object_id):
