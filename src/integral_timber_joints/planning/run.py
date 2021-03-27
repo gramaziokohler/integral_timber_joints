e9dac09a4444f75210b1d004cadd323c2b4188b0
Yijiang Huang <yijiangh@mit.edu>
2021-03-26 23:41:09 -0400

    seq 36

--- src/integral_timber_joints/planning/run.py
+++ src/integral_timber_joints/planning/run.py
@@ -184,7 +184,6 @@ def propagate_states(process, selected_movements, all_movements, options=None, p
             # get_movement_status(process, back_m, [RoboticMovement]) in [MovementStatus.has_traj, MovementStatus.both_done] and \
             elif back_m.trajectory is None or back_end_conf is None or \
                 compare_configurations(back_end_conf, target_start_conf, jump_threshold, verbose=False):
-
                 back_end_state['robot'].kinematic_config = target_start_conf
                 if plan_impacted:
                     back_m.trajectory = None
@@ -345,6 +344,7 @@ def compute_selected_movements(client, robot, process, beam_id, priority, moveme
         altered_new_movements, impact_movements = propagate_states(process, altered_movements, all_movements, options=options,
             plan_impacted=plan_impacted)
         altered_movements.extend(altered_new_movements)
+        altered_movements.extend(impact_movements)
 
         if plan_impacted and len(impact_movements) > 0:
             print('+'*10)
@@ -605,7 +605,7 @@ def main():
         # only one
         beam_ids = [process.assembly.sequence[args.seq_i]]
 
-    beam_attempts = 10 if args.batch_run else 50
+    beam_attempts = 10 if args.batch_run else 1
     for beam_id in beam_ids:
         print('-'*20)
         s_i = process.assembly.sequence.index(beam_id)
