c8548ba9cc2fa276f1c05646482ba4595be52dc3
Victor Leung <yck011522@gmail.com>
2021-04-30 17:10:57 +0200

    screw init

--- src/integral_timber_joints/geometry/__init__.py
+++ src/integral_timber_joints/geometry/__init__.py
@@ -5,3 +5,4 @@ from .joint import Joint
 from .joint_90lap import Joint_90lap, Joint_90lap_from_beam_beam_intersection
 from .joint_halflap import Joint_halflap, Joint_halflap_from_beam_beam_intersection
 from .joint_non_planar_lap import JointNonPlanarLap, non_planar_lap_joint_from_beam_beam_intersection
+from .screw import Screw_SL
