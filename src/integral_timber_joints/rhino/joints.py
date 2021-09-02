a48036a8bc054ffcfcd29f6d0bcbcfd8692077a5
Victor Leung <yck011522@gmail.com>
2021-09-03 00:48:20 +0200

    Add Rhino UI method to customize depth of lap joints Fixes #68

--- src/integral_timber_joints/rhino/joints.py
+++ src/integral_timber_joints/rhino/joints.py
@@ -3,15 +3,20 @@ import os
 
 import Rhino  # type: ignore
 import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg
+
 import scriptcontext as sc  # type: ignore
+from compas.geometry import Cylinder, Polyhedron
 from compas.utilities import DataDecoder
 from compas_rhino.geometry import RhinoMesh
 from compas_rhino.ui import CommandMenu
+from compas_rhino.utilities import clear_layer, delete_objects, draw_breps, draw_cylinders, draw_mesh
 from compas_rhino.utilities.objects import get_object_name, get_object_names
 
 from integral_timber_joints.assembly import Assembly
-from integral_timber_joints.geometry import EnvironmentModel, JointNonPlanarLap, Joint_halflap
+from integral_timber_joints.geometry import EnvironmentModel, Joint_halflap, JointNonPlanarLap
 from integral_timber_joints.process import Movement, RobotClampAssemblyProcess, RoboticMovement
+from integral_timber_joints.rhino.artist import mesh_to_brep, vertices_and_faces_to_brep_struct
 from integral_timber_joints.rhino.load import get_process, get_process_artist, process_is_none
 from integral_timber_joints.rhino.utility import get_existing_beams_filter, recompute_dependent_solutions
 from integral_timber_joints.tools import Clamp, Gripper, RobotWrist, ToolChanger
@@ -20,33 +25,186 @@ try:
     from typing import Dict, List, Optional, Tuple, cast
 except:
     pass
+add_brep = sc.doc.Objects.AddBrep
+find_object = sc.doc.Objects.Find
+
+# ######################################################
+# Functions tp draw, redraw and delete selectable joints
+# ######################################################
 
 
-def change_joint_parameters(process):
+def draw_selectable_joint(process, joint_id, redraw=True):
+    # type: (RobotClampAssemblyProcess, tuple[str,str],  bool) -> None
+
+    artist = get_process_artist()
+    if not hasattr(artist, '_joint_features'):
+        artist._joint_features = {}
+
+    # Collect all the feature shapes from the joint and Boolean Union into one object
+    rs.EnableRedraw(False)
+    joint = process.assembly.joint(joint_id)
+    beam = process.assembly.beam(joint_id[0])
+    shapes = joint.get_feature_shapes(beam)
+    guids_for_union = []
+    for shape in shapes:
+        if isinstance(shape, Polyhedron):
+            vertices_and_faces = shape.to_vertices_and_faces()
+            struct = vertices_and_faces_to_brep_struct(vertices_and_faces)
+            # print("Polyhedron :", struct)
+            guids_for_union.extend(draw_breps(struct, join=True, redraw=False))
+        elif isinstance(shape, Cylinder):
+            cylinder = shape
+            start = cylinder.center + cylinder.normal.scaled(cylinder.height / 2)
+            end = cylinder.center - cylinder.normal.scaled(cylinder.height / 2)
+            struct = {'start': list(start), 'end': list(end), 'radius': cylinder.circle.radius}
+            # print("Cylinder : ", struct)
+            guids_for_union.extend(draw_cylinders([struct], cap=True, redraw=False))
+    breps = [rs.coercebrep(guid) for guid in guids_for_union]
+    boolean_result = rg.Brep.CreateBooleanUnion(breps, sc.doc.ModelAbsoluteTolerance)
+    delete_objects(guids_for_union, purge=True, redraw=False)
+
+    if boolean_result is None:
+        print("ERROR: joints.py draw_joint_boolean_feature(%s) Boolean Failure" % joint_id)
+
+    # Add boolean result into Rhino Doc and save their guids
+    artist._joint_features[joint_id] = []
+    for brep in boolean_result:
+        # New guids from boolean results
+        guid = add_brep(brep)
+        if not guid:
+            continue
+        # Naming
+        rs.ObjectName(guid, "%s-%s" % (joint_id[0], joint_id[1]))
+        # Red Green Coloring
+        if process.assembly.sequence.index(joint_id[0]) < process.assembly.sequence.index(joint_id[1]):
+            rs.ObjectColor(guid, (30, 190, 85))
+        else:
+            rs.ObjectColor(guid, (250, 80, 60))
+        # Add to guid dict
+        artist._joint_features[joint_id].append(guid)
+
+    if redraw:
+        rs.EnableRedraw(True)
+
+
+def draw_all_selectable_joints(process, redraw=True):
+    for joint_id in process.assembly.joint_ids():
+        draw_selectable_joint(process, joint_id, redraw=False)
+    if redraw:
+        rs.EnableRedraw(True)
+
+
+def delete_selectable_joint(process, joint_id, redraw=True):
+    # type: (RobotClampAssemblyProcess, tuple[str,str], bool) -> None
+    artist = get_process_artist()
+    rs.EnableRedraw(False)
+    if joint_id in artist._joint_features:
+        delete_objects(artist._joint_features[joint_id], purge=True, redraw=False)
+    artist._joint_features[joint_id] = []
+    if redraw:
+        rs.EnableRedraw(True)
+
+
+def delete_all_selectable_joints(process, redraw=True):
+    # type: (RobotClampAssemblyProcess, bool) -> None
+    artist = get_process_artist()
+    rs.EnableRedraw(False)
+    for joint_id in artist._joint_features:
+        delete_selectable_joint(process, joint_id, redraw=False)
+    if redraw:
+        rs.EnableRedraw(True)
+
+
+def users_select_feature(process, joint_types=None):
+    # type: (RobotClampAssemblyProcess, list[type]) -> list[Tuple[str, str]]
+    # Menu for user
+    go = Rhino.Input.Custom.GetObject()
+    go.SetCommandPrompt("Select joints:")
+    go.EnablePreSelect(False, True)
+
+    # Create a list of selectable guids
+    artist = get_process_artist()
+    beam_guids = []
+    for joint_id in process.assembly.joint_ids():
+        joint = process.assembly.joint(joint_id)
+        if joint_types is not None:
+            if not any([isinstance(joint, joint_type) for joint_type in joint_types]):
+                continue
+        # Add the guids that have been drawn to the list
+        beam_guids.extend(artist._joint_features[joint_id])
+
+    # Set getObjects geometry filter
+    def joint_feature_filter(rhino_object, geometry, component_index):
+        return rhino_object.Attributes.ObjectId in beam_guids
+    go.SetCustomGeometryFilter(joint_feature_filter)
+
+    result = go.GetMultiple(0, 0)
+    if result is None:
+        return None
+
+    # Retrive joint_ids from object name
+    joint_ids = []
+    for object in go.Objects():
+        guid = object.ObjectId
+        name = get_object_name(guid)  # type: str
+        ids = name.split('-')
+        joint_ids.append((ids[0], ids[1]))
+    return joint_ids
+
+
+def cull_double_selected_joint_ids(process, joint_ids):
+    # type: (RobotClampAssemblyProcess, list[Tuple[str, str]]) -> list[Tuple[str, str]]
+    filtered_joint_ids = []
+    sequence = process.assembly.sequence
+    # Add joint_id that are earlier
+    for joint_id in joint_ids:
+        beam_id_0, beam_id_1 = joint_id
+        if sequence.index(beam_id_0) < sequence.index(beam_id_1):
+            filtered_joint_ids.append(joint_id)
+    # Add joint_id that are later, only if the earlier id is not in the list
+    for joint_id in joint_ids:
+        beam_id_0, beam_id_1 = joint_id
+        if sequence.index(beam_id_0) > sequence.index(beam_id_1):
+            if (beam_id_1, beam_id_0) not in filtered_joint_ids:
+                filtered_joint_ids.append(joint_id)
+    return filtered_joint_ids
+
+# ##############################
+# Functions to operate on Joints
+# ##############################
+
+
+def change_lap_joint_parameters(process):
     # type: (RobotClampAssemblyProcess) -> None
     artist = get_process_artist()
-    # Ask user to pick two beams
-    guid1 = rs.GetObject('Select first beam of joint for parameter change:', custom_filter=get_existing_beams_filter(process))
-    if guid1 is None:
-        return
-    beam_id1 = get_object_name(guid1)
-    guid2 = rs.GetObject('Select second beam of joint for parameter change:', custom_filter=get_existing_beams_filter(process, exclude_beam_ids=[beam_id1]))
-    if guid2 is None:
-        return
-    beam_id2 = get_object_name(guid2)
-    joint_id = (beam_id1, beam_id2)
 
-    if joint_id not in process.assembly.joint_ids():
-        print("Joint (%s-%s) does not exist." % (beam_id1, beam_id2))
-    joint = process.assembly.joint(joint_id)
-    print("Joint (%s-%s) (%s)" % (beam_id1, beam_id2, joint.__class__.__name__))
+    # Ask user to pick two joints
+    joint_ids = users_select_feature(process, joint_types=[Joint_halflap])
+    joint_ids = cull_double_selected_joint_ids(process, joint_ids)
+    if len(joint_ids) == 0:
+        return
 
-    affected_beams = set()
-    if isinstance(joint, Joint_halflap):
+    # Print out current joint parameters
+    existing_heights = set()
+    for joint_id in joint_ids:
+        joint = process.assembly.joint(joint_id)
         current_height = joint.height
-        new_height = rs.GetReal("New thickness of the lap joint: (current is %s)" % current_height, current_height)
-        if new_height is None or new_height == current_height:
-            return
+        existing_heights.add(current_height)
+        print("Joint (%s-%s) height = %s" % (joint_id[0], joint_id[1], current_height))
+
+    # Ask user for new paramter value
+    new_height = rs.GetReal("New thickness of the lap joint: (Existing Heights are: %s" % existing_heights)
+    if new_height is None:
+        return
+
+    # Make changes to all the selected joints
+    affected_beams = set()
+    for joint_id in joint_ids:
+        beam_id1, beam_id2 = joint_id
+        joint = process.assembly.joint(joint_id)
+        current_height = joint.height
+        if new_height == current_height:
+            continue
         difference = new_height - current_height
         process.assembly.joint((beam_id1, beam_id2)).height += difference
         process.assembly.joint((beam_id2, beam_id1)).height -= difference
@@ -56,9 +214,17 @@ def change_joint_parameters(process):
         ))
         affected_beams.add(beam_id1)
         affected_beams.add(beam_id2)
+        # Redraw new selectable joint feature
+        delete_selectable_joint(process, (beam_id1, beam_id2))
+        delete_selectable_joint(process, (beam_id2, beam_id1))
+        draw_selectable_joint(process, (beam_id1, beam_id2))
+        draw_selectable_joint(process, (beam_id2, beam_id1))
 
+    # Redraw beams with new joints
     for beam_id in affected_beams:
         artist.redraw_interactive_beam(beam_id, force_update=True, redraw=False)
+
+    # Redraw new selectable joint feature
     rs.EnableRedraw(True)
 
 
@@ -76,8 +242,17 @@ def show_menu(process):
     [artist.hide_asstool_all_positions(beam_id) for beam_id in assembly.sequence]
     [artist.hide_gripper_all_positions(beam_id) for beam_id in assembly.sequence]
     [artist.show_interactive_beam(beam_id) for beam_id in assembly.sequence]
+    draw_all_selectable_joints(process, redraw=False)
     rs.EnableRedraw(True)
     sc.doc.Views.Redraw()
+
+    # On exist function
+    def on_exit_ui():
+        delete_all_selectable_joints(process)
+        print('Exit Function')
+        return Rhino.Commands.Result.Cancel
+
+    command_to_run = None
 
     def construct_menu():
         # Menu for user
@@ -86,7 +261,7 @@ def show_menu(process):
             'options': [
                 {'name': 'Finish', 'action': 'Exit'
                  },
-                {'name': 'JointParameters', 'action': change_joint_parameters
+                {'name': 'LapJoint', 'action': change_lap_joint_parameters
                  },
             ]
         }
@@ -101,10 +276,6 @@ def show_menu(process):
 
         # Create Menu
         result = CommandMenu(construct_menu()).select_action()
-
-        def on_exit_ui():
-            print('Exit Function')
-            return Rhino.Commands.Result.Cancel
 
         # User cancel command by Escape
         if result is None:
