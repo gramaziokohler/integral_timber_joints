8a7aaeb3d2c881bd266723905b650709f81e55f8
Yijiang Huang <yijiangh@mit.edu>
2021-11-01 15:31:58 -0400

    use pyplanner for fluent-problem

--- src/integral_timber_joints/planning/load_pddlstream.py
+++ src/integral_timber_joints/planning/load_pddlstream.py
@@ -12,6 +12,18 @@ except KeyError:
        os.path.abspath(os.path.join(HERE, '..', '..', '..', 'external', 'pddlstream'))
     ])
 
+try:
+    sys.path.append(os.environ['PYPLANNERS_PATH'])
+except KeyError:
+    cprint('No `PYPLANNERS_PATH` found in the env variables, using pyplanner submodule', 'yellow')
+    here = os.path.abspath(os.path.dirname(__file__))
+    pyplanner_path = os.path.abspath(os.path.join(here, '..', '..', 'external', 'pyplanners'))
+    # Inside PDDLStream, it will look for 'PYPLANNERS_PATH'
+    os.environ['PYPLANNERS_PATH'] = pyplanner_path
+
 import pddlstream
 cprint("Using pddlstream from {}".format(pddlstream.__file__), 'yellow')
+
+import strips # pyplanners
+cprint("Using strips (pyplanners) from {}".format(strips.__file__), 'yellow')
 
